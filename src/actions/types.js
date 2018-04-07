f2fa746396b79f99261fb6382b8032bda1719e3c
Queen Bae <sashakyunghwa@gmail.com>
2018-04-06 17:45:43 -0700

    completed form with redux

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,9 @@
 export default {
-    GET_LIST_DATA: 'get_list_data'
+    GET_LIST_DATA: 'get_list_data',
+    ADD_ITEM: 'add_item'
 };
+
+
 
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import listReducer from './list_reducer';
 
 export default combineReducers({
-    list: listReducer
+    list: listReducer,
+    form: formReducer
 });
 
